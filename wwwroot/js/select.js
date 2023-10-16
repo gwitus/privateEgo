f0eb2e632545aa5ba1f0c93d9571a684e1577293
gwitus <emanuelbarbosati@gmail.com>
2023-10-16 01:04:46 -0300

    Salvo modo com select

--- wwwroot/js/select.js
+++ wwwroot/js/select.js
@@ -2,8 +2,14 @@ const select  = document.getElementById("optionsNav");
 const select2 = document.getElementById("optionsNav2");
 const select3 = document.getElementById("optionsNav3");
 
+// options
+let optionXP = document.getElementById("xp");
+
 select.addEventListener("click", () => {
-    select.classList.toggle("open");
+    // select.classList.toggle("open");
+    select.style.marginBottom = '160px';
+    select.style.outline = "0";
+    optionXP.style.outline = '0';
 });
 
 select2.addEventListener("click", () => {
