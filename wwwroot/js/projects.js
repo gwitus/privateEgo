c3e2e622dd619dd1a36aaa0ea38be8d14018c3ef
gwitus <emanuelbarbosati@gmail.com>
2023-10-16 05:22:32 -0300

    estrutura do nav terminado de forma porca, ajustes serão feitos futuramente

--- wwwroot/js/projects.js
+++ wwwroot/js/projects.js
@@ -1,28 +1,43 @@
+// variaveis globais
+console.log("Documento carregado!");
 const projetos = document.getElementById("projectCSharp");
 const java = document.getElementById("JAVA");
 const web = document.getElementById("WEB1");
 const C = document.getElementById("C");
-// dando problema no I, tentar adpátar o hamburguer
-// const i        = document.getElementById("seta2");
+const projectsMain = document.getElementById("projects");
+// var trazidas
+var optionAbout = document.getElementById("optionMe");
+var optionAboutSkill = document.getElementById("optionMe1");
+const optionAboutMe = document.getElementById("optionMeAbout");
+const i = document.getElementById("seta");
+const seta = document.getElementById("seta2");
+const contato = document.getElementById("contatoDiv");
+const sourceCode = document.getElementById("sourceCode");
 
-console.log("Documento carregado!");
-
-// #Define Java equals none display
+// #Define or set value on pertinent VAR'S
 java.style.display = "none";
 web.style.display = "none";
 C.style.display = "none";
+optionAbout.style.display = "none";
+optionAboutSkill.style.display = "none";
 
-// Função funcionando muito melhor
-function validarProjects() {
-    if (java.style.display == "none") {
-        openProjects();
-    } else {
-        closeProjects();
-    }
+// functions
+function openEgo() {
+    // optionAboutMe.style.transition = "transition: all 1s ease-in-out;";
+    optionAbout.style.display = "block";
+    optionAbout.style.top = "5rem";
+    optionAboutSkill.style.display = "block";
+    optionAboutSkill.style.top = "9rem"
+    i.style.transform = "rotate(0.75turn)";
+    // contato.style.top = "12.8rem";
 }
 
 function closeEgo() {
-
+    // contato.style.top = "6rem";
+    optionAboutMe.style.marginBottom = "0rem";
+    optionAbout.style.display = "none";
+    optionAboutSkill.style.display = "none";
+    i.style.transform = "rotate(0turn)";
 }
 
 function openProjects() {
@@ -32,10 +47,34 @@ function openProjects() {
     web.style.top = "10rem";
     C.style.display = "block";
     C.style.top = "15rem";
+    seta.style.transform = "rotate(0.75turn)";
 }
 
 function closeProjects() {
     java.style.display = "none";
     C.style.display = "none";
     web.style.display = "none";
+    seta.style.transform = "rotate(0turn)";
+}
+
+
+// Função funcionando muito melhor
+function egoHability() {
+    if (optionAbout.style.display == "none" && java.style.display == "none"){
+        openEgo();
+        projectsMain.style.paddingTop = "7rem";
+    } else {
+        closeEgo();
+        projectsMain.style.paddingTop = "0px";
+    }
+}
+
+function projectsHability() {
+    if (optionAbout.style.display == "none" && java.style.display == "none"){
+        openProjects();
+        sourceCode.style.display = "none";
+    } else {
+        closeProjects();
+        sourceCode.style.display = "block";
+    }
 }
